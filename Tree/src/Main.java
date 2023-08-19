2d193171a6bddcdc174b38c48b69a770b24a3034
Kay Tang <ktan185@aucklanduni.ac.nz>
2023-08-19 13:43:46 +1200

    feat: added script that allows user to insert or delete from tree.

--- Tree/src/Main.java
+++ Tree/src/Main.java
@@ -8,29 +8,47 @@ public class Main {
 
     Scanner sc = new Scanner(System.in);
     AVLTree tree = new AVLTree();
+    boolean isOneNode = false;
+    boolean choice = true;
     
     while (true) {
-      System.out.println("Choose an integer/s to insert: ");
+      if (!isOneNode) {
+        System.out.println("Choose integer/s to insert: ");
+      } else {
+        System.out.println("Would you like to insert or Delete a node? I/D");      
+        String response = sc.nextLine();
+        response = response.toLowerCase();  // Fix: Assign the lowercase version to response
+        if (response.startsWith("d")) {
+          System.out.println("Choose integer/s to delete: ");
+          choice = false;
+        } else {
+          choice = true;
+        }
+      }      
       String val = sc.nextLine();
       try {
         String[] array = val.split(" ");
         for (String s : array) {
-            int key = Integer.parseInt(s);
-            tree.insert(key);
+          int key = Integer.parseInt(s);
+          chooseMethod(tree, key, choice);
         }
         TreePrinter.print(tree.root);
+        isOneNode = true;
       } catch (Exception e) {
         System.out.println("Input invalid, please try again.");
       }
       System.out.println("Would you like to exit: y/n");
-				String yesNo = sc.nextLine();
-				yesNo = yesNo.toLowerCase();
-			  if (yesNo.startsWith("y")) {
-					break;
-			  }
-      
+      String yesNo = sc.nextLine();
+      yesNo = yesNo.toLowerCase();
+      if (yesNo.startsWith("y")) {
+        break;
+      } 
     }
     sc.close();
   }
 
+  private static void chooseMethod(AVLTree tree, int key, boolean option) {
+    if (option == true) tree.insert(key); 
+    else tree.delete(key);
+  }
 }
